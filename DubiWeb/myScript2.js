8f5019cb36f6d8102ba2a5effb3cafe701eece18
GWC20 <gwc20@GWC20s-MacBook-Air.local>
2018-08-01 09:48:16 -0400

    Initial commit

---
+++ DubiWeb/myScript2.js
@@ -0,0 +1,29 @@
+/*function pascalfunction(){
+  document.getElementById("pascal").innerHTML = "Pascal is a fun loving cat!";
+}
+document.getElementById("pascal").onclick = function() {pascalfunction()};
+
+
+
+
+
+function kikifunction(){
+  document.getElementById("kiki").innerHTML = "Kiki likes to kill birds!";
+}
+document.getElementById("kiki").onclick = function() {kikifunction()};
+
+
+
+
+
+function igorfunction(){
+  document.getElementById("igor").innerHTML = "Igor hates Dubi!";
+}
+document.getElementById("igor").onclick = function() {igorfunction()};
+
+
+
+function alfiefunction(){
+  document.getElementById("alfie").innerHTML = "Alfie likes to nap!";
+}
+document.getElementById("alfie").onclick = function() {alfiefunction()};*/
